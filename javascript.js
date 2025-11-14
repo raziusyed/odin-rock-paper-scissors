8531b6838c213457e2aa5bb0ea89dfa9ddd3a623
Razi <raziusyed@gmail.com>
2025-11-14 12:47:18 -0500

    Implement game function

--- javascript.js
+++ javascript.js
@@ -13,51 +13,60 @@ function getHumanChoice() {
     return prompt("Choose!");
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
 
-    if (humanChoice === computerChoice) {
-        console.log("It's a draw! Play again.");
-        return;
+        if (humanChoice === computerChoice) {
+            console.log("It's a draw! Play again.");
+            return;
+        }
+
+        switch (humanChoice) {
+            case "rock":
+                if (computerChoice === "paper") {
+                    console.log("You lose! Paper beats Rock");
+                    computerScore++;
+                } else {
+                    console.log("You win! Rock beats Scissors");
+                    humanScore++;
+                }
+                break;
+            case "paper":
+                if (computerChoice === "scissors") {
+                    console.log("You lose! Scissors beats Paper");
+                    computerScore++;
+                } else {
+                    console.log("You win! Paper beats Rock");
+                    humanScore++;
+                }
+                break;
+            default:
+                if (computerChoice === "rock") {
+                    console.log("You lose! Rock beats Scissors");
+                    computerScore++;
+                } else {
+                    console.log("You win! Scissors beats Paper");
+                    humanScore++;
+                }
+        }
     }
 
-    switch (humanChoice) {
-        case "rock":
-            if (computerChoice === "paper") {
-                console.log("You lose! Paper beats Rock");
-                computerScore++;
-            } else {
-                console.log("You win! Rock beats Scissors");
-                humanScore++;
-            }
-            break;
-        case "paper":
-            if (computerChoice === "scissors") {
-                console.log("You lose! Scissors beats Paper");
-                computerScore++;
-            } else {
-                console.log("You win! Paper beats Rock");
-                humanScore++;
-            }
-            break;
-        default:
-            if (computerChoice === "rock") {
-                console.log("You lose! Rock beats Scissors");
-                computerScore++;
-            } else {
-                console.log("You win! Scissors beats Paper");
-                humanScore++;
-            }
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (computerScore > humanScore) {
+        console.log(`You lose! The computer won ${computerScore} times. You won ${humanScore} times.`);
+    } else if (humanScore > computerScore) {
+        console.log(`You win! You won ${humanScore} times. The computer won ${computerScore} times.`);
+    } else {
+        console.log(`It's a draw! You won ${humanScore} times. The computer won ${computerScore} times.`);
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
-
+playGame();
 
