947bfa564b34118a1b61440575a41200b608797e
Razi <raziusyed@gmail.com>
2025-11-14 12:35:18 -0500

    Write logic for playing a round

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 function getComputerChoice() {
     const number = Math.random();
-    console.log(number);
-    
+
     if (number > 0.66) {
         return "rock";
     } else if (number > 0.33) {
@@ -11,8 +10,54 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    const choice = prompt("Choose!");
+    return prompt("Choose!");
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw! Play again.");
+        return;
+    }
+
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "paper") {
+                console.log("You lose! Paper beats Rock");
+                computerScore++;
+            } else {
+                console.log("You win! Rock beats Scissors");
+                humanScore++;
+            }
+            break;
+        case "paper":
+            if (computerChoice === "scissors") {
+                console.log("You lose! Scissors beats Paper");
+                computerScore++;
+            } else {
+                console.log("You win! Paper beats Rock");
+                humanScore++;
+            }
+            break;
+        default:
+            if (computerChoice === "rock") {
+                console.log("You lose! Rock beats Scissors");
+                computerScore++;
+            } else {
+                console.log("You win! Scissors beats Paper");
+                humanScore++;
+            }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
 
 
