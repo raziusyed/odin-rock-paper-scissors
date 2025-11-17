07d61b07b92c0d3ca33317b6fc9a8057844a3510
Razi <raziusyed@gmail.com>
2025-11-17 16:48:57 -0500

    Add ui

--- javascript.js
+++ javascript.js
@@ -16,55 +16,86 @@ function getHumanChoice() {
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    let result = "";
+    let round = 0;
 
     function playRound(humanChoice, computerChoice) {
         humanChoice = humanChoice.toLowerCase();
 
         if (humanChoice === computerChoice) {
-            console.log("It's a draw! Play again.");
+            result = "It's a draw! Play again.";
             return;
         }
 
         switch (humanChoice) {
             case "rock":
                 if (computerChoice === "paper") {
-                    console.log("You lose! Paper beats Rock");
+                    result = "You lose! Paper beats Rock";
                     computerScore++;
                 } else {
-                    console.log("You win! Rock beats Scissors");
+                    result = "You win! Rock beats Scissors";
                     humanScore++;
                 }
                 break;
             case "paper":
                 if (computerChoice === "scissors") {
-                    console.log("You lose! Scissors beats Paper");
+                    result = "You lose! Scissors beats Paper";
                     computerScore++;
                 } else {
-                    console.log("You win! Paper beats Rock");
+                    result = "You win! Paper beats Rock";
                     humanScore++;
                 }
                 break;
             default:
                 if (computerChoice === "rock") {
-                    console.log("You lose! Rock beats Scissors");
+                    result = "You lose! Rock beats Scissors";
                     computerScore++;
                 } else {
-                    console.log("You win! Scissors beats Paper");
+                    result = "You win! Scissors beats Paper";
                     humanScore++;
                 }
         }
     }
 
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
+    function appendResult() {
+        const roundResult = document.createElement("p");
+        roundResult.textContent = `Round ${++round} result: ${result}, Your Score: ${humanScore}, Computer Score: ${computerScore}`;
+        results.appendChild(roundResult);
+        if (round == 5) checkWinner();
     }
 
-    if (computerScore > humanScore) {
-        console.log(`You lose! The computer won ${computerScore} times. You won ${humanScore} times.`);
-    } else if (humanScore > computerScore) {
-        console.log(`You win! You won ${humanScore} times. The computer won ${computerScore} times.`);
-    } else {
-        console.log(`It's a draw! You won ${humanScore} times. The computer won ${computerScore} times.`);
+    const rockBtn = document.querySelector("#rock");
+    const paperBtn = document.querySelector("#paper");
+    const scissorsBtn = document.querySelector("#scissors");
+    const results = document.querySelector("#results");
+
+    rockBtn.addEventListener("click", () => {
+        playRound("rock", getComputerChoice());
+        appendResult();
+    });
+
+    paperBtn.addEventListener("click", () => {
+        playRound("paper", getComputerChoice());
+        appendResult();
+    });
+
+    scissorsBtn.addEventListener("click", () => {
+        playRound("scissors", getComputerChoice());
+        appendResult();
+    });
+
+    function checkWinner() {
+        const gameResult = document.createElement("p");
+
+        if (computerScore > humanScore) {
+            gameResult.textContent = `You lose! The computer won ${computerScore} times. You won ${humanScore} times.`;
+        } else if (humanScore > computerScore) {
+            gameResult.textContent = `You win! You won ${humanScore} times. The computer won ${computerScore} times.`;
+        } else {
+            gameResult.textContent = `It's a draw! You won ${humanScore} times. The computer won ${computerScore} times.`;
+        }
+
+        results.appendChild(gameResult);
     }
 }
 
